fa8e921cd212a0b0da41442b62a63344160e09e7
tontsa28 <xdman7917@gmail.com>
2021-02-10 16:23:48 +0200

    added GUI.java

---
+++ src/GUI.java
@@ -0,0 +1,53 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GUI implements ActionListener{
+
+    int count = 0;
+    JLabel label;
+    JFrame frame;
+    JPanel panel;
+
+
+    public GUI() {
+
+        frame = new JFrame();
+
+        JButton button = new JButton("Paina tästä!");
+        button.addActionListener(this);
+
+        label = new JLabel("Klikkaukset: 0");
+
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("GUI");
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Klikkaukset: " + count);
+
+    }
+    
+}
